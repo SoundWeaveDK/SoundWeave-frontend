a1468b84773d48733cf3d53b8edeed99d4d7613b
kristoffer888 <kristofferclausen@rocketmail.com>
2023-09-28 13:51:31 +0200

    cut point

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -4,7 +4,7 @@ const { locale } = useI18n()
 
 <template >
     <div class="flex overflow-y-auto ">
-        <div class="flex flex-col h-screen p-3 bg-white shadow w-60">
+        <div class="flex flex-col h-screen p-3 text-black dark:text-white  bg-white dark:bg-slate-900 w-60">
             <div class="space-y-3">
                 <div>
                     <!-- logo -->
@@ -29,11 +29,34 @@ const { locale } = useI18n()
                             </p>
                         </li>
                         <!-- followed creators -->
-                        <div v-for="creators in testCreators">
-                            <button class="flex py-2">
-                                <img :src="creators.creator_image" class="w-10 h-10 rounded-full" />
-                                <span class="text-sm my-auto px-2 ">{{ creators.creator_name }}</span>
-                            </button>
+                        <div v-for="(creators, index) in testCreators">
+                            <div v-if="index == testCreators.length - 1">
+                                <div v-if="followMore">
+                                    <button class="flex py-2">
+                                        <img :src="creators.creator_image" class="w-10 h-10 rounded-full" />
+                                        <p class="text-sm my-auto px-2 ">{{ creators.creator_name }}</p>
+                                    </button>
+                                </div>
+                                <button class="flex py-2" @click="toggleFollow()">
+                                    <Icon v-if="followMore" name="ic:baseline-keyboard-arrow-up" />
+                                    <Icon v-else name="ic:baseline-keyboard-arrow-down" />
+                                    <p v-if="followMore" class="text-sm my-auto px-2 hover:text-gray-500 ">{{
+                                        $t('showLess') }}</p>
+                                    <p v-else class="text-sm my-auto px-2 hover:text-gray-500 ">{{ $t('showMore') }}</p>
+                                </button>
+                            </div>
+                            <div v-else-if="index < 4">
+                                <button class="flex py-2">
+                                    <img :src="creators.creator_image" class="w-10 h-10 rounded-full" />
+                                    <p class="text-sm my-auto px-2 ">{{ creators.creator_name }}</p>
+                                </button>
+                            </div>
+                            <div v-else-if="followMore">
+                                <button class="flex py-2">
+                                    <img :src="creators.creator_image" class="w-10 h-10 rounded-full" />
+                                    <p class="text-sm my-auto px-2 ">{{ creators.creator_name }}</p>
+                                </button>
+                            </div>
                         </div>
                         <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                         <li class="rounded-sm">
@@ -50,7 +73,6 @@ const { locale } = useI18n()
                                 </button>
                             </a>
                             <div v-for="(lists, index) in testLists">
-                                <!-- if index is greater tahn 4 then add a "show more" button -->
                                 <div v-if="index == testLists.length - 1">
                                     <div v-if="collectionMore">
                                         <button class="flex py-2">
