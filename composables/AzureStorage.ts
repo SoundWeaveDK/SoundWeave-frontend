f319ac504bc7f5ba0e99dd8ac940223305303358
frede <frederik@rovi.dk>
2023-10-06 17:49:05 +0200

    upload now works

--- composables/AzureStorage.ts
+++ composables/AzureStorage.ts
@@ -1,47 +1,47 @@
-import { DefaultAzureCredential } from "@azure/identity";
-import { StorageSharedKeyCredential, BlobServiceClient } from "@azure/storage-blob";
+import { BlobServiceClient } from "@azure/storage-blob";
 import axios from "~/utils/axiosInstance";
 
-const PRODUCTION: boolean = process.env.NODE_ENV === 'production';
 
-const AZURITEACCOUNTKEY: any = process.env.AZURITEACCOUNTKEY;
-
-// devstoreaccount1 is the default account name created for Azurite emulator.
-const ACCOUNTNAME = PRODUCTION ? "soundweavestorage" : "devstoreaccount1";
-// The service endpoints for Azurite are different from the endpoints of an Azure Storage account. The local computer doesn't do domain name resolution, requiring Azurite endpoints to be local addresses.
-const ACCOUNTURL = PRODUCTION
-  ? `https://${ACCOUNTNAME}.blob.core.windows.net`
-  : `http://127.0.0.1:10000/${ACCOUNTNAME}`;
-
-
-export const UploadImage = async (fileName: string, content: ArrayBuffer) => {
+export const UploadImage = async (fileName: string, file: any) => {
   try {
-    const response = await axios.post("/api/azurestorage/getuploadimagesastoken", {
-      fileName: fileName,
+
+    const response = await axios.post("/api/azurestorage/getuploadsastoken", {
+      containername: "images",
     });
 
     const sasToken = response.data;
 
-    console.log("fileName:", fileName);
-    console.log("sasToken:", sasToken);
-    console.log("content:", content);
+    const blobServiceClient = new BlobServiceClient(sasToken);
 
-    let auth = null;
-    if (PRODUCTION) {
-      // Use managed identity in production for our static web app
-      auth = new DefaultAzureCredential();
-    } else {
-      // Use Azurite account key in development
-      auth = new StorageSharedKeyCredential(ACCOUNTNAME, AZURITEACCOUNTKEY);
-    }
+    const containerClient = blobServiceClient.getContainerClient("images");
 
-    const blockBlobClient = new BlobServiceClient(ACCOUNTURL, auth).getContainerClient("images").getBlockBlobClient(fileName + sasToken);
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
 
-    await blockBlobClient.uploadData(content);
-    console.log("UploadImage success");
+    return await blockBlobClient.uploadBrowserData(file);
 
   } catch (error) {
     console.error("UploadImage error:", error);
   }
 }
+
+export const UploadPodcast = async (fileName: string, file: any) => {
+  try {
+    const response = await axios.post("/api/azurestorage/getuploadsastoken", {
+      containername: "podcasts",
+    });
+
+    const sasToken = response.data;
+
+    const blobServiceClient = new BlobServiceClient(sasToken);
+
+    const containerClient = blobServiceClient.getContainerClient("podcasts");
+
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+
+    return await blockBlobClient.uploadBrowserData(file);
+
+  } catch (error) {
+    console.error("UploadImage error:", error);
+  }
+};
 
--- pages/AzureStorageExample.vue
+++ pages/AzureStorageExample.vue
@@ -3,9 +3,10 @@
     <h1 class="text-4xl text-center">Azure Storage Example</h1>
     <div class="flex justify-center">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="uploadFile">
-        Upload File
+        Upload image
       </button>
     </div>
+    <h1 class="text-4xl text-center">Upload status: {{ uploadStatus }}</h1>
   </div>
 </template>
 
@@ -16,18 +17,18 @@ export default {
     async uploadFile() {
       const input = document.createElement('input');
       input.type = 'file';
-      input.onchange = e => {
-        // getting a hold of the file reference
+      input.onchange = async e => {
         var file = e.target.files[0];
-        // read as buffer to get the raw bytes and log to console
-        var reader = new FileReader();
-        reader.readAsArrayBuffer(file);
-
-        reader.onloadend = async function (evt) {
-          UploadImage(evt.target.result, file.name);
-        }
+        var response = await UploadImage(file.name, file)
+        console.log(response)
+        this.uploadStatus = response._response.status == 201 ? 'Successfully uploaded' : 'Upload failed'
       }
       input.click();
+    }
+  },
+  data() {
+    return {
+      uploadStatus: 'Not started'
     }
   }
 }
