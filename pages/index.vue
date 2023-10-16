b46d33f2025e011de3a5d87419f500d8423e726c
Isaac Vorköper <isaaclennart@gmail.com>
2023-10-16 09:05:05 +0200

    Page switch around.

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,21 @@
 <template>
-    <div class=" h-screen p-4">
-        <h1 class="lg:pl-28 mobile:text-center text-xl text-black dark:text-white">{{ $t('yourFeed') }}</h1>
-        <PodcastBox v-if="podcastData" :podcastData="podcastData" />
-    </div>
+    <PodcastBox :podcastData="podcastData" />
 </template>
-
 <script>
 import axios from '@/utils/axiosInstance.ts'
-import { useUserStore } from "../stores/login"
-import { mapStores } from "pinia";
 
 export default {
-    name: 'Home',
-    setup() {
-        const { locale } = useI18n()
-        return { locale }
-    },
-    computed: {
-        ...mapStores(useUserStore)
-    },
-    created() {
-        this.token = this.userStore.getAccessToken;
-        this.loggedInUser = this.userStore.getUser;
-        if (this.loggedInUser.id) {
-            this.getFollowing();
-            this.getPodcasts();
-        }
-    },
     data() {
         return {
-            token: '',
-            loggedInUser: [],
-            followed: [],
             podcastData: [],
-        }
+        };
+    },
+    created() {
+        this.getUserPodcasts();
     },
     methods: {
-        async getFollowing() {
-            // get the user's following
-            await axios.get('/api/followuser/read-users-followers/' + this.loggedInUser.id, {
-                headers: {
-                    Authorization: `Bearer ${this.token}`
-                }
-            }).then((response) => {
-                this.followed = [];
-                for (let i = 0; i < response.data.length; i++) {
-                    for (let j = 0; j < response.data[i].following.length; j++) {
-                        this.followed.push(response.data[i].following[j]);
-                    }
-                }
-            }).catch((error) => {
-                if (error) {
-                    alert((error));
-                }
-            });
-        },
-        async getPodcasts() {
-            // get the podcasts of the users the logged in user follows
-            await axios.get('/api/podcast/read-following-podcast/' + this.loggedInUser.id, {
-                headers: {
-                    Authorization: `Bearer ${this.token}`
-                }
+        async getUserPodcasts() {
+            await axios.get('/api/podcast/read-preview-podcasts', {
             }).then((response) => {
                 this.podcastData = response.data;
             }).catch((error) => {
@@ -70,6 +25,5 @@ export default {
             });
         },
     },
-}
-
+};
 </script>
